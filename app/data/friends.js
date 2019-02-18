a7cc47a384091a5676aba0762bc7abcb86057781
Jordan Rabideau <jordanmrabideau@gmail.com>
2019-02-18 18:49:44 -0500

    Can now push data to the api

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var friends = [
+    {
+        name: "Cthulhu",
+        image: "https://upload.wikimedia.org/wikipedia/commons/6/62/Cthulhu_and_R%27lyeh.jpg",
+        scores: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    }
+]
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -6,3 +6,10 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+require(path.join(__dirname, './app/routing/apiRoutes.js'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes.js'))(app);
+
+app.listen(PORT, function() {
+    console.log("Listening on port: " + PORT)
+})

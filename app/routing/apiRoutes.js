a95482982e88af99e71f0738b73c1696484c7398
Jordan Rabideau <jordanmrabideau@gmail.com>
2019-02-19 15:18:26 -0500

    API can now check to see who is the most compatable.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,9 +6,34 @@ module.exports = function(app) {
     })
     
     app.post("/api/friends", function(req, res){
-        var newFriend = req.body;
-        friends.push(newFriend)
-        res.json(newFriend);
+        var user = req.body;
+        for (i in user.scores) {
+            user.scores[i] = parseInt(user.scores[i]);
+        }
+        var differenceArray = [];
+        function compatCheck() {
+            for (i in friends) {
+                var totalDifference = 0
+                for (j in friends[i].scores) {
+                    var difference = Math.abs(friends[i].scores[j] - user.scores[j]);
+                    totalDifference += difference;
+                }
+                differenceArray.push(totalDifference)
+            }
+            var minDif = Math.min(...differenceArray);
+            console.log("minimum difference: " + minDif);
+            var matchIndex = differenceArray.indexOf(minDif)
+            console.log("match index: " + matchIndex);
+            var match = friends[matchIndex];
+            console.log(match);
+            
+        }
+        compatCheck();
+        friends.push(user)
+        res.json(match);
+        
     })
+
+
 }
 

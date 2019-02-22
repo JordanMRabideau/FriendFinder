3622169321a5291fdc6085357430da977e37e679
Jordan Rabideau <jordanmrabideau@gmail.com>
2019-02-21 20:01:40 -0500

    added some comments on the apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,23 +10,28 @@ module.exports = function(app) {
         var match;
         for (i in user.scores) {
             user.scores[i] = parseInt(user.scores[i]);
-        } 
+        }
+
         var differenceArray = [];
+        
+        // This function takes the user's input and compares that to the scores of preexisting friends
         function compatCheck() {
             for (i in friends) {
                 var totalDifference = 0
+                // The difference between the user's score and the friend's score is calculated by adding the difference of each question
+                // the total difference for each 'friend' is pushed into differenceArray 
                 for (j in friends[i].scores) {
                     var difference = Math.abs(friends[i].scores[j] - user.scores[j]);
                     totalDifference += difference;
                 }
                 differenceArray.push(totalDifference)
             }
+            // The minimum total difference is calculated by using the Math.min() method.
+            // The index of the minumum difference will correspond to the index of the friend that it belongs to in the friends array.
+            // This index is then used to find the friend with the lowest difference and saves that friend as the match.
             var minDif = Math.min(...differenceArray);
-            console.log("minimum difference: " + minDif);
             var matchIndex = differenceArray.indexOf(minDif)
-            console.log("match index: " + matchIndex);
             match = friends[matchIndex];
-            console.log(match);
             
         }
         compatCheck();
